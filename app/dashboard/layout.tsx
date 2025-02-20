730c40346e36af26f9c4ebeba107291e8f747fc7
isgr9801 <sgrp9801@gmail.com>
2025-02-21 03:01:26 +0530

    test

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { AuthProvider } from "../../components/AuthProvider";
-import "../../styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
